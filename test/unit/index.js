c0373f3ef4557af25b8da212e40030da0d323d97
林建辉 <390720046@qq.com>
2017-04-25 18:02:10 +0800

    test: add karma test

---
+++ test/unit/index.js
@@ -0,0 +1,3 @@
+const testsContext = require.context('./specs', true, /\.spec$/)
+testsContext.keys().forEach(testsContext)
+
---
+++ test/unit/util.js
@@ -0,0 +1,98 @@
+import Vue from '../../node_modules/vue/dist/vue.esm.js';
+import VueDragging from '../../vue-dragging.js'
+
+Vue.use(VueDragging)
+
+let id = 0;
+
+const createElm = function() {
+  const elm = document.createElement('div');
+
+  elm.id = 'app' + ++id;
+  document.body.appendChild(elm);
+
+  return elm;
+}
+
+/**
+ * 回收 vm
+ * @param  {Object} vm
+ */
+export const destroyVM = function(vm) {
+  vm.$el &&
+  vm.$el.parentNode &&
+  vm.$el.parentNode.removeChild(vm.$el);
+};
+
+/**
+ * 创建一个 Vue 的实例对象
+ * @param  {Object|String}  Compo   组件配置，可直接传 template
+ * @param  {Boolean=false} mounted 是否添加到 DOM 上
+ * @return {Object} vm
+ */
+export const createVue = function(Compo, mounted = false) {
+  if (Object.prototype.toString.call(Compo) === '[object String]') {
+    Compo = { template: Compo };
+  }
+  return new Vue(Compo).$mount(mounted === false ? null : createElm());
+};
+
+/**
+ * 创建一个测试组件实例
+ * @link http://vuejs.org/guide/unit-testing.html#Writing-Testable-Components
+ * @param  {Object}  Compo          - 组件对象
+ * @param  {Object}  propsData      - props 数据
+ * @param  {Boolean=false} mounted  - 是否添加到 DOM 上
+ * @return {Object} vm
+ */
+export const createTest = function(Compo, propsData = {}, mounted = false) {
+  if (propsData === true || propsData === false) {
+    mounted = propsData;
+    propsData = {};
+  }
+  const elm = createElm();
+  const Ctor = Vue.extend(Compo);
+  return new Ctor({ propsData }).$mount(mounted === false ? null : elm);
+};
+
+/**
+ * 触发一个事件
+ * mouseenter, mouseleave, mouseover, keyup, change, click 等
+ * @param  {Element} elm
+ * @param  {String} name
+ * @param  {*} opts
+ */
+export const triggerEvent = function(elm, name, ...opts) {
+    if (typeof CustomEvent === 'function') {
+        const evt = new CustomEvent(name, ...opts);
+        elm.dispatchEvent(evt);
+    } else {
+        let eventName;
+        if (/^mouse|click/.test(name)) {
+        eventName = 'MouseEvents';
+        } else if (/^key/.test(name)) {
+        eventName = 'KeyboardEvent';
+        } else {
+        eventName = 'HTMLEvents';
+        }
+        const evt = document.createEvent(eventName);
+
+        evt.initEvent(name, ...opts);
+        elm.dispatchEvent
+        ? elm.dispatchEvent(evt)
+        : elm.fireEvent('on' + name, evt);
+    }
+    return elm;
+};
+
+/**
+ * 触发 “mouseup” 和 “mousedown” 事件
+ * @param {Element} elm
+ * @param {*} opts
+ */
+export const triggerClick = function(elm, ...opts) {
+  triggerEvent(elm, 'mousedown', ...opts);
+  triggerEvent(elm, 'mouseup', ...opts);
+
+  return elm;
+};
